507876942c2afcf5a9c27072cda6acec1079419f
Mark Ghian Francis <zetsuya6167@gmail.com>
2024-06-29 23:10:07 +0800

    added folder to separate the files

---
+++ js/tictactoe.js
@@ -0,0 +1,32 @@
+const gameBoard = ( function() {
+  const board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ]
+
+  //DOM cache
+  
+  //Bind Events
+
+  
+  
+  //Console Game Based
+  function consoleBased() {
+    
+  }
+
+  function askPlayerName() {}
+
+  function askPlayGame() {}
+
+  function askInput() {}
+
+  function consoleTTT() {}
+
+  function drawBoardConsole() {}
+
+  function updateBoardConsole() {}
+  
+  function tttSolution() {}
+})();
